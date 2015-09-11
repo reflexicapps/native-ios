dbf964adb469e98f076bea023a684be62c81ea0d
Gogs <gogs@fake.local>
2015-09-11 15:57:07 +0300

    Made events dispatching changes in order to reduce the memory garbage generated

--- tealeaf/js/jsTimestep.mm
+++ tealeaf/js/jsTimestep.mm
@@ -22,68 +22,109 @@
 #include "gen/js_timestep_image_map_template.gen.h"
 #include "js/js_timestep_view.h"
 
+
+
+using namespace JS;
+
 static JSObject *evt_ctor = NULL;
 
-static bool defGetEvents(JSContext *cx, unsigned argc, jsval *vp) {
-	JS_BeginRequest(cx);
-
-	input_event_list events = timestep_events_get();
-	int len = events.count;
-	JSObject *arr = JS_NewArrayObject(cx, len, NULL);
-	if (!evt_ctor) {
-		jsval thiz_val = JS_THIS(cx, vp);
-		JSObject *thiz = JSVAL_TO_OBJECT(thiz_val);
-    JS::RootedValue ctor_val(cx);
-		JS_GetProperty(cx, thiz, "InputEvent", &ctor_val);
-		if (!JSVAL_IS_PRIMITIVE(ctor_val)) {
-			JSObject *ctor_obj = JSVAL_TO_OBJECT(ctor_val);
-			if (ctor_obj && JS_ObjectIsFunction(cx, ctor_obj)) {
-				evt_ctor = ctor_obj;
-				JS_AddObjectRoot(cx, &evt_ctor);
-			}
-		}
-	}
-	JS_ASSERT(evt_ctor != NULL);
-	for (int i = 0; i < len; i++) {
-		input_event evt = events.events[i];
-		jsval x, y, type, id;
-		x = INT_TO_JSVAL(evt.x);
-		y = INT_TO_JSVAL(evt.y);
-		type = INT_TO_JSVAL(evt.type);
-		id = INT_TO_JSVAL(evt.id);
-
-		jsval argv[4] =	 {id, type, x, y};
-		JSObject *evt_obj = JS_New(cx, evt_ctor, 6,argv);
-    JS::RootedValue evt_val(cx, JS::ObjectValue(*evt_obj));
-		JS_SetElement(cx, arr, i, &evt_val);
-	}
-	JS_SET_RVAL(cx, vp, OBJECT_TO_JSVAL(arr));
-
-	JS_EndRequest(cx);
-	return true;
+static JSBool defGetEvents(JSContext *cx, unsigned argc, jsval *vp) {
+    JS_BeginRequest(cx);
+    
+    input_event_list events = timestep_events_get();
+    int len = events.count;
+    
+    //
+    //Get handle of InputEvent JS object contructor
+    //
+    jsval thiz_val = JS_THIS(cx, vp);
+    JSObject *thiz = JSVAL_TO_OBJECT(thiz_val);
+    
+    if (!evt_ctor)
+    {
+    	JS::RootedValue ctor_val(cx); //WARNING: Try JS_RootedValue if fails
+        JS_GetProperty(cx, thiz, "InputEvent", &ctor_val);
+        if (!JSVAL_IS_PRIMITIVE(ctor_val)) {
+            JSObject *ctor_obj = JSVAL_TO_OBJECT(ctor_val);
+            if (ctor_obj && JS_ObjectIsFunction(cx, ctor_obj)) {
+                evt_ctor = ctor_obj;
+                JS_AddObjectRoot(cx, &evt_ctor);
+            }
+        }
+    }
+    
+    jsval arr_val;
+    jsval eventsLenVal;
+    jsval targetEventVal;
+    jsval x, y, type, id;
+    jsval initMethodVal;
+    jsval tmpVal;
+    
+    jsval argv[4];
+    
+    JS_GetProperty(cx, thiz, "GlobalInputEventsList", &arr_val);
+    
+    JSObject *arr = JSVAL_TO_OBJECT(arr_val);
+    
+    JS_ASSERT(evt_ctor != NULL);
+    
+    
+    for (int i = 0; i < len; i++)
+    {
+        input_event evt = events.events[i];
+        
+        
+        x = INT_TO_JSVAL(evt.x);
+        y = INT_TO_JSVAL(evt.y);
+        type = INT_TO_JSVAL(evt.type);
+        id = INT_TO_JSVAL(evt.id);
+        
+        JS_GetElement(cx, arr, i, &targetEventVal);
+        JSObject *targetEvent = JSVAL_TO_OBJECT(targetEventVal);
+        
+        if( i == 0 )
+        {
+            JS_GetProperty(cx, targetEvent, "init", &initMethodVal);
+        }
+        
+        argv[0] = id;
+        argv[1] = type;
+        argv[2] = x;
+        argv[3] = y;
+        
+        JS_CallFunctionName(cx, targetEvent, "init", 4,argv, &tmpVal);
+    }
+    
+    jsval lenVal = INT_TO_JSVAL(len);
+    JS_SetProperty(cx, thiz, "GlobalInputEventsListLen", &lenVal);
+    
+    JS_SET_RVAL(cx, vp, OBJECT_TO_JSVAL(arr));
+    
+    JS_EndRequest(cx);
+    return JS_TRUE;
 }
 
 static const JSFunctionSpec functionSpec[] = {
-	JS_FN("getEvents",				defGetEvents,				0, FUNCTION_FLAGS),
-	JS_FN("setImageOnImageView",	def_image_view_set_image,	3, FUNCTION_FLAGS),
-	JS_FS_END
+    JS_FN("getEvents",				defGetEvents,				0, FUNCTION_FLAGS),
+    JS_FN("setImageOnImageView",	def_image_view_set_image,	3, FUNCTION_FLAGS),
+    JS_FS_END
 };
 
 
 @implementation jsTimestep
 
 + (void) addToRuntime:(js_core *)js {
-	JSObject *timestep = JS_NewObject(js.cx, NULL, NULL, NULL);
-	JS_DefineProperty(js.cx, js.native, "timestep", OBJECT_TO_JSVAL(timestep), NULL, NULL, PROPERTY_FLAGS);
-	JS_DefineFunctions(js.cx, timestep, (JSFunctionSpec*)functionSpec);
-
-	timestep_view_add_to_object(timestep);
-	timestep_image_map_add_to_object(timestep);
-	animate_add_to_object(timestep);
+    JSObject *timestep = JS_NewObject(js.cx, NULL, NULL, NULL);
+    JS_DefineProperty(js.cx, js.native, "timestep", OBJECT_TO_JSVAL(timestep), NULL, NULL, PROPERTY_FLAGS);
+    JS_DefineFunctions(js.cx, timestep, (JSFunctionSpec*)functionSpec);
+    
+    timestep_view_add_to_object(timestep);
+    timestep_image_map_add_to_object(timestep);
+    animate_add_to_object(timestep);
 }
 
 + (void) onDestroyRuntime {
-	evt_ctor = NULL;
+    evt_ctor = NULL;
 }
 
 @end
